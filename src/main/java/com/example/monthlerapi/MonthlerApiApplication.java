2d19bb0600f0b4fdd247c32657121c307511c589
uz2ni <uzini_@naver.com>
2022-12-24 23:48:01 +0900

    첫 생성

---
+++ src/main/java/com/example/monthlerapi/MonthlerApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.monthlerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonthlerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonthlerApiApplication.class, args);
+	}
+
+}
